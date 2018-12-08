8d582f795db2da62dacca6f6d60fee029f03ddf3
Bashir Shallah <bashir.shallah@gmail.com>
2018-12-08 11:00:50 +0300

    working with controlled inputs.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    value: 'Default'
+  }
+
+  setValue = (event) => {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input value={this.state.value} onChange={this.setValue} />
       </div>
     );
   }
